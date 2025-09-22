0f850b608dddbbcd195b8d80ea398f95dd863f19
seoyonara <syp614@ewhain.net>
2025-09-23 03:07:21 +0900

    🎉 프로젝트 시작

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173",],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the TableTalk"}
